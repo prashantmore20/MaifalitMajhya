86f2691bb66dde84d1dd2dff3244596b584b9c2d
prashantmore20 <prashant.more20@gmail.com>
2021-06-20 21:44:03 +0530

    designed home page

--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin
+from blog.models import Post
 
 # Create your views here.
 
 class Index(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'landing/index.html')
+        logged_in_user = request.user
+        posts = Post.objects.filter().order_by('-timeStamp')[:3]
+
+        context = {
+            'post_list': posts,
+        }
+        
+        return render(request, 'landing/index.html', context)
